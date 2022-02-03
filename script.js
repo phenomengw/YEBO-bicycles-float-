a01108d76d5442a31b05a92caec804331c6bf524
Kirill Tamilo <95633425+phenomengw@users.noreply.github.com>
2022-02-03 09:46:44 +0300

    update js & html

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
 
-    //Задержка появления окна Subscribe//
-    $(".subscribe").hide(0).delay(3000).show(0);
+    //Скрытие прелоадера
+    $('.preloader_anim_1, .preloader_anim_2, .preloader_bg').delay(2000).fadeOut();
     
+    //Появление окна Subscribe//
+    $(".subscribe").delay(5000).show(0);
+    $('html, body').delay(5000).css('overflow-y', 'hidden');
     
     //Закрытие окна Subscribe//
-    $(".subscribe_close").click(function() {
+    $(".subscribe_close, .subscribe_bg").click(function() {
         $(".subscribe").hide(0);
+        $('html, body').css('overflow-y', 'inherit');
     });
     
     //Появление окна при отправке формы//
-    $(".subscribe_button").click(function() {
+    $(".subscribe_button, #submit").click(function() {
         $(".you_are_subscribe").delay(1000).show(0).delay(5000).hide(0);
         $(".subscribe").hide(0)
     });
     
     //Добавление адреса в ссобщение о подписке//
-    $(".subscribe_input").change(function() {
+    $(".subscribe_input, #input_field").change(function() {
         $(".subscribe_mail").text($(this).val());
     });
 
@@ -37,7 +41,7 @@ $(document).ready(function() {
         $(".retro_right").delay(200).animate({"opacity": 1}, 500);
     });
     
-     $(".twit_text").waypoint(function() {
+     $(".twit").waypoint(function() {
         $(".shop_left_top_left").animate({"opacity": 1}, 500);
         $(".shop_left_top_right").delay(200).animate({"opacity": 1}, 500);
         $(".shop_center_left").delay(400).animate({"opacity": 1}, 500);
@@ -47,4 +51,47 @@ $(document).ready(function() {
         $(".shop_right_bottom_left").delay(1200).animate({"opacity": 1}, 500);
         $(".shop_right_bottom_right").delay(1000).animate({"opacity": 1}, 500);
     });
+    
+    //Слайдер твиттов//
+    const slider = $(".owl-carousel").owlCarousel({
+        items: 4,
+        margin: 20,
+        loop: false,
+        nav: false,
+        dots: true,
+        mouseDrag: true,
+        touchDrag: true,
+        responsive: {
+            300: {
+                items: 1,
+        },
+            750: {
+                items: 2,
+        },
+            900: {
+                items: 2,
+        },
+            1250: {
+                items: 3
+        },
+            1700: {
+                items: 4
+        }
+        }
+    });
+    
+    //Трансформации панели навигации//
+        $(window).scroll(function () {
+            display = $(this).scrollTop() >= 100;
+            if(display){
+                $("#logo").fadeOut(0);
+                $(".arrow").fadeIn(0);
+                $("#top_nav").removeClass("nav_scroll");
+            }
+            else {
+                $(".arrow").fadeOut(0);
+                $("#logo").fadeIn(0);
+                $("#top_nav").addClass("nav_scroll");
+            }
+        });
 })
